b81620061232253b93c7d6290bef9fab9c673a4c
unknown <nicole.moguilevskaia@mail.utoronto.ca>
2021-09-04 16:01:02 -0400

    added bmi calculations

--- calculator.js
+++ calculator.js
@@ -9,8 +9,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
-
-
+app.get('/bmicalculator', function(req, res){
+    res.sendFile(__dirname + '/bmiCalculator.html');
+});
 
 // post
 app.post("/", function(req, res){
@@ -18,6 +19,12 @@ app.post("/", function(req, res){
     var num2 = Number(req.body.num2);
     var sol = num1 + num2;
     res.send(`The answer is ${sol}`);
+});
+app.post("/bmicalculator", function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight/(height * height);
+    res.send(`Your BMI is ${bmi}`);
 });
 
 // request
